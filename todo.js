e93945c7cde7ae52215d8d14ab7703d2de55dcb1
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-04-15 02:56:12 +0500

    Adding CLI based to_do list

---
+++ todo.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todowork = [];
+let statements = true;
+console.log(chalk.blue.bold("\n \t HELLO THERE! WHICH TASKS ARE FOR TODAY? \n"));
+while (statements) {
+    let addtasks = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.gray.bold("ENTER YOUR TODAY'S NEW TASK:")
+        }
+    ]);
+    todowork.push(addtasks.task);
+    console.log(`${addtasks.task} YOUR TASK ADDED SUCCESFULLY!`);
+    let moretask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: chalk.gray.bold("DO YOU WANNA ADD MORE TASK?"),
+            default: "False"
+        }
+    ]);
+    statements = moretask.addmore;
+}
+console.log(chalk.green("YOUR ALL TO-DO TASK FOR TODAY :", todowork));
